6732933b206fc13e2f5fced2c8ff6b78e008f30c
Lisias T <me@lisias.net>
2018-11-09 17:51:36 -0200

    Legal mambo jambo

--- Source/PartCommander/Properties/AssemblyInfo.cs
+++ Source/PartCommander/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PartCommanderContinued")]
+[assembly: AssemblyTitle("Part Commander /L Unofficial")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sean McDougall")]
-[assembly: AssemblyProduct("PartCommanderContinued")]
-[assembly: AssemblyCopyright("Copyright © Sean McDougall 2015")]
+[assembly: AssemblyProduct("PartCommander")]
+[assembly: AssemblyCopyright("© Sean McDougall 2015, 2018 LisiasT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
